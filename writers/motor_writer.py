422760fdc8129dddcd41890eeaafc47575bcb2d4
Anton Pollak <busse37@gmx.de>
2017-08-11 16:09:45 +0200

    Fifth attempt to write data

--- writers/motor_writer.py
+++ writers/motor_writer.py
@@ -1,7 +1,7 @@
 from writers.motor import default_motor
 from writers.writer import Writer
 
-velocity = 0.4
+velocity = 0.6
 class MotorWriter(Writer):
     def __enter__(self):
         self.motor = default_motor()
