383acdb9750b574cb4fd5517b32a3f0f5d28cb64
Anton Pollak <busse37@gmx.de>
2017-08-12 10:22:06 +0200

    Seventh attmpt to collect data

--- pipes/shrinking.py
+++ pipes/shrinking.py
@@ -4,11 +4,11 @@ import cv2
 
 class Shrinking(Pipe):
     def pipe(self, data):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        r = 100.0 / gray.shape[1]
-        dim = (100, int(gray.shape[0] * r))
-        gray = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
-        return gray
+        data = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        r = 100.0 / data.shape[1]
+        dim = (100, int(data.shape[0] * r))
+        data = cv2.resize(data, dim, interpolation = cv2.INTER_AREA)
+        return data
 
     def __enter__(self):
         return self
